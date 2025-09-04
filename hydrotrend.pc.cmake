9e06baeddb2675cd0a2e040342159e26f89d4953
Mark Piper <mark.piper@colorado.edu>
2025-09-04 13:37:41 -0600

    Don't use cmake variables

--- hydrotrend.pc.cmake
+++ hydrotrend.pc.cmake
@@ -2,4 +2,4 @@ Name: LibHydrotrend
 Description: Hydrotrend library
 Version: ${HYDROTREND_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_hydrotrend
-Cflags: -I${CMAKE_INSTALL_INCLUDEDIR} -I${CMAKE_INSTALL_INCLUDEDIR}/hydrotrend
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include -I${CMAKE_INSTALL_PREFIX}/include/hydrotrend
