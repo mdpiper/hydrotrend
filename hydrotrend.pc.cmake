14e15cb132aef4603e0b2ff1e1d8f7dc23ddcf96
Mark Piper <mark.piper@colorado.edu>
2025-09-04 13:35:33 -0600

    Use a different cmake variable for include path

--- hydrotrend.pc.cmake
+++ hydrotrend.pc.cmake
@@ -2,4 +2,4 @@ Name: LibHydrotrend
 Description: Hydrotrend library
 Version: ${HYDROTREND_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_hydrotrend
-Cflags: -I${CMAKE_INSTALL_FULL_INCLUDEDIR} -I${CMAKE_INSTALL_FULL_INCLUDEDIR}/hydrotrend
+Cflags: -I${CMAKE_INSTALL_INCLUDEDIR} -I${CMAKE_INSTALL_INCLUDEDIR}/hydrotrend
